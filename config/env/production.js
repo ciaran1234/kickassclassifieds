9668ac9df96646b1f7050bda71955c0b576c0b64
ciaran1234 <ciaran.mcdonald4@gmail.com>
2018-01-13 01:27:11 +0000

    Updated Production Configuration

--- config/env/production.js
+++ config/env/production.js
@@ -7,19 +7,29 @@ module.exports = {
         certificate: './config/sslcerts/cert.pem'
     },
     db: {
-        uri: 'mongodb://localhost/mean-dev',
+        uri: 'mongodb://ciaran_admin:Elecmag1!@cluster0-shard-00-00-6fvrq.mongodb.net:27017,cluster0-shard-00-01-6fvrq.mongodb.net:27017,cluster0-shard-00-02-6fvrq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
         credentials: {
             user: '',
             password: ''
-        }
+        },
+        debug: false
+    },
+    port: 80,
+    cache: {
+        host: 'localhost',
+        port: '6379'
     },
     app: {
-        title: ''
+        title: 'Kick Ass Classifieds'
     },
+    sessionSecret: 'sdkflskfkl;234!!x_sjksdklkj+(xx',
     facebook: {
-        clientID: process.env.FACEBOOK_ID || 'APP_ID',
-        clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-        callbackURL: '/api/auth/facebook/callback'
+        clientID: process.env.FACEBOOK_ID || '431431603869202',
+        clientSecret: process.env.FACEBOOK_SECRET || '124031dacc0f40ebf6b5119514eba438',
+        callbackURL: '/api/auth/facebook/callback',
+        scope: { scope: ['email'] },
+        appToken: '431431603869202|89vVaoi1TOX4sKG3yLKgpK9xt-8',
+        verificationUrl: 'https://graph.facebook.com/debug_token?input_token=#{clientAccessToken}&access_token=#{appToken}'
     },
     twitter: {
         clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
@@ -27,9 +37,11 @@ module.exports = {
         callbackURL: '/api/auth/twitter/callback'
     },
     google: {
-        clientID: process.env.GOOGLE_ID || 'APP_ID',
-        clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
-        callbackURL: '/api/auth/google/callback'
+        clientID: process.env.GOOGLE_ID || '449715294481-peuaa6413388nfn0utf1uosv1lleu3ti.apps.googleusercontent.com',
+        clientSecret: process.env.GOOGLE_SECRET || 'nR0DxFfoFsugzxA-l84tWZpd',
+        callbackURL: '/api/auth/google/callback',
+        scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'],
+        verificationUrl: 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=#{clientAccessToken}'
     },
     linkedin: {
         clientID: process.env.LINKEDIN_ID || 'APP_ID',
@@ -46,5 +58,10 @@ module.exports = {
         clientSecret: process.env.PAYPAL_SECRET || 'CLIENT_SECRET',
         callbackURL: '/api/auth/paypal/callback',
         sandbox: true
-    }
+    },    
+    cors: {
+        enabled: true,
+        allowedOrigins: ['http://localhost:4200']
+    },
+    whitelistUrls: ['http://localhost:4200']
 };
